b41514e1dee338f9874a4961cd8f8f5b40570cac
wolfsup7 <laurendooley@gmail.com>
2023-01-25 16:00:53 -0500

    up to serializers

---
+++ django_rest_api/clients_api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length=32)
+    breed = models.CharField(max_length=32)
+    age = models.IntegerField()
+    picture = models.CharField(max_length=250)
+    address = models.CharField(max_length=50)
+    phone = models.CharField(max_length=32)
+    appointment = models.CharField(max_length=32)
