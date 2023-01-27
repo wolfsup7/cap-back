9654494a00ad79dbe3ec826948c2dbd3f67eafec
wolfsup7 <laurendooley@gmail.com>
2023-01-27 00:47:00 -0500

    asdasd

--- django_rest_api/django_rest_api/settings.py
+++ django_rest_api/django_rest_api/settings.py
@@ -95,9 +95,6 @@ DATABASES = {
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
-REST_FRAMEWORK = {
-    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
-}
 
 
 # Password validation
@@ -136,7 +133,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
